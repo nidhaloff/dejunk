ee73f2d1ab73280c537f20af3fa7ae507eb8b5d7
nidhal baccouri <nidhalbacc@gmail.com>
2021-05-10 13:25:52 +0200

    release 0.1.1

--- dejunk/__init__.py
+++ dejunk/__init__.py
@@ -3,4 +3,4 @@ from dejunk.dejunk import Dejunk
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email='nidhalbacc@gmail.com',
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/dejunk',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
